be64045e1199c0928080c851a3571ee20ff6bdaf
Aldona <aldonapawlak2@gamil.com>
2020-11-24 09:37:24 +0100

    initial commit

---
+++ src/main/java/com/codecool/coffeeshopspringrestapi/CoffeeShopSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.coffeeshopspringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopSpringRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeShopSpringRestApiApplication.class, args);
+    }
+
+}
